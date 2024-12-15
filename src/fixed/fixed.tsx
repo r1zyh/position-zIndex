0cfa796cfb46d16ecb8ffd0c75fe970de5a37e6e
r1zyh <kirill.ananiev2012@yandex.ru>
2024-12-15 23:48:54 +0300

    Добавит конспект в README. Доделает странички Relative и Fixed.

--- src/fixed/fixed.tsx
+++ src/fixed/fixed.tsx
@@ -1,10 +1,25 @@
 import { Header } from "../header";
 
+// fixed - позиционирование происходит относительно окна браузера, за исключением случаев,
+// когда св-ва transition, perspective или filter имеют значения отличные от none.
+// fixed как и  absolute уходит из основного потока документа.
+
 export function Fixed(): JSX.Element {
   return (
-    <>
+    <div className="container__page fixed">
       <Header />
-      <h1>Fixed</h1>
-    </>
+      <div className="container__unique fixed">
+        <div className="group">
+          <h1 className="title fixed">Fixed</h1>
+          <p className="text">This is some text template</p>
+          <p className="text">This is some text template</p>
+          <p className="text">This is some text template</p>
+          <p className="text">This is some text template</p>
+          <p className="text">This is some text template</p>
+          <p className="text">This is some text template</p>
+          <p className="text">This is some text template</p>
+        </div>
+      </div>
+    </div>
   );
 }
--- src/relative/relative.tsx
+++ src/relative/relative.tsx
@@ -1,10 +1,27 @@
 import { Header } from "../header";
 
+// relative - позиционирование происходит так же как и static, 
+// но элемент может быть смещен относительно своего начального положения. 
+// Он не будет влиять на расположение других элементов, кроме случая, 
+// когда элемент выходит за пределы границы своего родителя имеющего прокрутку.
+
+
 export function Relative(): JSX.Element {
   return (
-    <>
+    <div className="container__page">
       <Header />
-      <h1>Relative</h1> 
-    </>
+      <div className="container__unique relative">
+        <div className="group">
+          <h1 className="title relative">Relative</h1>
+          <p className="text">This is some text template</p>
+          <p className="text">This is some text template</p>
+          <p className="text">This is some text template</p>
+          <p className="text">This is some text template</p>
+          <p className="text">This is some text template</p>
+          <p className="text">This is some text template</p>
+          <p className="text">This is some text template</p>
+        </div>
+      </div>
+    </div>
   );
 }
